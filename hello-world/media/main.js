2acc430e9d2f190581532ff093e42ff67110249c
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-08-06 13:55:35 -0700

    Update comments in hello-world/media/main.js to increase clarity

--- hello-world/media/main.js
+++ hello-world/media/main.js
@@ -1,8 +1,9 @@
-// Get access to the VS Code API from within this webview context
+// Get access to the VS Code API from within the webview context
 const vscode = acquireVsCodeApi();
 
 // Just like a regular webpage we need to wait for the webview
-// to load before we can reference any of the components
+// DOM to load before we can reference any of the HTML elements 
+// or toolkit components
 window.addEventListener("load", main);
 
 function main() {
