5882a5db71c36e91e629b79dc99534515405c00c
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-09-10 13:33:44 -0700

    Remove primary badge code

--- all-components/src/ui/components/badge.ts
+++ all-components/src/ui/components/badge.ts
@@ -3,10 +3,6 @@ export const badgeDemo = /*html*/ `
     <h2>Badge</h2>
     <section class="component-example">
       <p>Default Badge</p>
-      <vscode-badge appearance="primary">1</vscode-badge>
-    </section>
-    <section class="component-example">
-      <p>Secondary Badge</p>
       <vscode-badge appearance="secondary">1</vscode-badge>
     </section>
   </section>
