a45ab51753fcbf64a23ea9d54d3e088481d1fbc0
Hawk Ticehurst <39639992+hawkticehurst@users.noreply.github.com>
2021-09-15 11:56:23 -0700

    Delete file from `out` directory

--- notepad/out/utilities/getUri.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getUri = void 0;
-const vscode_1 = require("vscode");
-function getUri(webview, extensionUri, pathList) {
-    return webview.asWebviewUri(vscode_1.Uri.joinPath(extensionUri, ...pathList));
-}
-exports.getUri = getUri;
-//# sourceMappingURL=getUri.js.map
