29eaf20489af7a96384aba61b32cab4232f6d954
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-09-15 18:01:13 -0700

    Fix formatting error in getWebviewContent HTML

--- hello-world/src/ui/getWebviewContent.ts
+++ hello-world/src/ui/getWebviewContent.ts
@@ -15,11 +15,11 @@ export function getWebviewContent(webview: Webview, extensionUri: Uri) {
 		<!DOCTYPE html>
 		<html lang="en">
 			<head>
-					<meta charset="UTF-8">
-					<meta name="viewport" content="width=device-width, initial-scale=1.0">
-					<script type="module" src="${toolkitUri}"></script>
-					<script type="module" src="${mainUri}"></script>
-					<title>Hello World!</title>
+				<meta charset="UTF-8">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<script type="module" src="${toolkitUri}"></script>
+				<script type="module" src="${mainUri}"></script>
+				<title>Hello World!</title>
 			</head>
 			<body>
 				<h1>Hello World!</h1>
