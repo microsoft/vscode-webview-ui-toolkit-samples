aedf26c1d0f10d9b84d9a399891b40e068169890
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-09-24 12:37:59 -0700

    Remove determinate state progress ring

--- all-components/src/ui/components/progress-ring.ts
+++ all-components/src/ui/components/progress-ring.ts
@@ -5,13 +5,5 @@ export const progressRingDemo = /*html*/ `
       <p>Default Progress Bar</p>
       <vscode-progress-ring></vscode-progress-ring>
     </section>
-    <section class="component-example">
-      <p>With Value</p>
-      <vscode-progress-ring value="65"></vscode-progress-ring>
-    </section>
-    <!--
-    <vscode-progress-ring min="10" value="5"></vscode-progress-ring>
-    <vscode-progress-ring max="75" value="100"></vscode-progress-ring>
-    -->
   </section>
 `;
