5469d71ad61e6b290ebc67929ceb66dd3ddf307d
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-09-27 21:34:52 -0700

    Increase clarity of getUri TSDoc comment

--- hello-world/src/utilities/getUri.ts
+++ hello-world/src/utilities/getUri.ts
@@ -1,7 +1,7 @@
 import { Uri, Webview } from "vscode";
 
 /**
- * A helper function which will get the URI of a given file or resource.
+ * A helper function which will get the webview URI of a given file or resource.
  *
  * @remarks This URI can be used within a webview's HTML as a link to the
  * given file/resource.
