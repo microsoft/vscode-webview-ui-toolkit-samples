bccf41d0aae0a6b529ac86a732fc3da1552cb7ca
David Dossett <ddossett@microsoft.com>
2021-10-05 15:00:38 -0700

    Initial commit (#30)

--- all-components/src/ui/components/progress-ring.ts
+++ all-components/src/ui/components/progress-ring.ts
@@ -2,7 +2,7 @@ export const progressRingDemo = /*html*/ `
   <section class="component-container">
     <h2>Progress Ring</h2>
     <section class="component-example">
-      <p>Default Progress Bar</p>
+      <p>Default Progress Ring</p>
       <vscode-progress-ring></vscode-progress-ring>
     </section>
   </section>
