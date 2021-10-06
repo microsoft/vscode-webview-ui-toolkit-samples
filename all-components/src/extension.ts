7d76f85c3758927f0f4c5eb15241a4b50b21654f
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-10-05 21:47:57 -0700

    Move webview creation code from the activate function into the AllComponentsPanel class

--- all-components/src/extension.ts
+++ all-components/src/extension.ts
@@ -1,36 +1,12 @@
-/**
- * This sample extension aims to keep code structure tidy by only importing exactly
- * what we need from the vscode package and moving the getWebviewContent function
- * into a separate ui directory.
- */
-import { commands, ExtensionContext, WebviewPanel, window, ViewColumn } from "vscode";
-import { getWebviewContent } from "./ui/getWebviewContent";
+import { commands, ExtensionContext } from "vscode";
+import { AllComponentsPanel } from "./panels/AllComponentsPanel";
 
 export function activate(context: ExtensionContext) {
-  let panel: WebviewPanel | undefined;
+  // Create the allcomponents command
+  const allComponentsCommand = commands.registerCommand("all-components.showAllComponents", () => {
+    AllComponentsPanel.render(context.extensionUri);
+  });
 
-  const allComponentsDisposable = commands.registerCommand(
-    "all-components.showAllComponents",
-    () => {
-      if (panel) {
-        // If the webview panel already exists reveal it
-        panel.reveal(ViewColumn.One);
-      } else {
-        // If a webview panel does not already exist create and show a new one
-        panel = window.createWebviewPanel(
-          "showAllComponents",
-          "Webview UI Toolkit: All Components",
-          ViewColumn.One,
-          {
-            enableScripts: true,
-          }
-        );
-
-        // Set the HTML content for the new webview panel
-        panel.webview.html = getWebviewContent(panel.webview, context.extensionUri);
-      }
-    }
-  );
-
-  context.subscriptions.push(allComponentsDisposable);
+  // Add command to the extension context
+  context.subscriptions.push(allComponentsCommand);
 }
