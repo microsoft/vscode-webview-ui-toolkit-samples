bcf93a45f1b3ccccd95f2b85675442ace13f3aed
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-10-06 13:27:54 -0700

    Move NotepadDataProvider class into a new providers directory

---
+++ notepad/src/providers/NotepadDataProvider.ts
@@ -0,0 +1,49 @@
+import { Event, EventEmitter, ProviderResult, ThemeIcon, TreeDataProvider, TreeItem } from "vscode";
+import { Note } from "../extension";
+
+type TreeDataOnChangeEvent = NotepadNote | undefined | null | void;
+
+export class NotepadDataProvider implements TreeDataProvider<NotepadNote> {
+  private _onDidChangeTreeData = new EventEmitter<TreeDataOnChangeEvent>();
+  readonly onDidChangeTreeData: Event<TreeDataOnChangeEvent> = this._onDidChangeTreeData.event;
+
+  data: NotepadNote[];
+
+  constructor(notesData: Note[]) {
+    this.data = notesData.map((note) => new NotepadNote(note.id, note.title));
+  }
+
+  refresh(notesData: Note[]): void {
+    this._onDidChangeTreeData.fire();
+    this.data = notesData.map((note) => new NotepadNote(note.id, note.title));
+  }
+
+  getTreeItem(element: NotepadNote): TreeItem | Thenable<TreeItem> {
+    return element;
+  }
+
+  getChildren(element?: NotepadNote | undefined): ProviderResult<NotepadNote[]> {
+    if (element === undefined) {
+      return this.data;
+    }
+    return element.children;
+  }
+
+  getParent() {
+    return null;
+  }
+}
+
+class NotepadNote extends TreeItem {
+  children?: NotepadNote[];
+
+  constructor(noteId: string, noteTitle: string) {
+    super(noteTitle);
+    this.id = noteId;
+    this.iconPath = new ThemeIcon("note");
+    this.command = {
+      title: "Open note",
+      command: "notepad.showNoteDetailView",
+    };
+  }
+}
