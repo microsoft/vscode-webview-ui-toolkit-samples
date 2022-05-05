1f8d5954d88bf72b75a4955f4b22f32d560fabe6
Hawk Ticehurst <hawkticehurst@gmail.com>
2022-05-05 13:44:52 -0700

    Create hello world Angular sample extension

---
+++ frameworks/hello-world-angular/webview-ui/src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+
+import { AppComponent } from "./app.component";
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+export class AppModule {}
