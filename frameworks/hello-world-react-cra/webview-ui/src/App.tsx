f28c8d4afe29c38ee10bb2807e901884ad729103
Hawk Ticehurst <39639992+hawkticehurst@users.noreply.github.com>
2022-12-14 16:54:45 -0800

    Add missing App.css file to hello world react + cra sample (#125)
    Description of changes
    
    Adds a missing App.css file back to the React + CRA hello world sample extension.

--- frameworks/hello-world-react-cra/webview-ui/src/App.tsx
+++ frameworks/hello-world-react-cra/webview-ui/src/App.tsx
@@ -1,5 +1,6 @@
 import { vscode } from "./utilities/vscode";
 import { VSCodeButton } from "@vscode/webview-ui-toolkit/react";
+import "./App.css";
 
 function App() {
   function handleHowdyClick() {
