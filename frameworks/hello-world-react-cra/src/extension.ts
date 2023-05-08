39ba5db3c95a1aef5ef62c2767235168537a2f6a
markwallace-microsoft <mark.e.wallace@outlook.com>
2023-05-08 15:47:31 +0100

    Add Router

--- frameworks/hello-world-react-cra/src/extension.ts
+++ frameworks/hello-world-react-cra/src/extension.ts
@@ -1,12 +1,17 @@
-import { commands, ExtensionContext } from "vscode";
+import { ExtensionContext, commands } from "vscode";
+
 import { HelloWorldPanel } from "./panels/HelloWorldPanel";
 
 export function activate(context: ExtensionContext) {
   // Create the show hello world command
   const showHelloWorldCommand = commands.registerCommand("hello-world.showHelloWorld", () => {
-    HelloWorldPanel.render(context.extensionUri);
+    HelloWorldPanel.render(context.extensionUri, "showHelloWorld", "Hello World");
+  });
+  const showHalloWeltCommand = commands.registerCommand("hello-world.showHalloWelt", () => {
+    HelloWorldPanel.render(context.extensionUri, "showHalloWelt", "Hallo-Welt");
   });
 
   // Add command to the extension context
   context.subscriptions.push(showHelloWorldCommand);
+  context.subscriptions.push(showHalloWeltCommand);
 }
